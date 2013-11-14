ecb2669a16ebf2bc2cd07c5f37ba57e0e06a88ad
David Hazinski <dhazinski@gmail.com>
2013-11-13 19:04:19 -0500

    Switching to base 61 hash.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from flask import abort, Flask, redirect, render_template, request, url_for
 from pymongo import MongoClient
 
 
-MONGO_URL = os.environ.get('MONGOHQ_URL', 'mongodb://127.0.0.1:27017/shortner')
+MONGO_URL = os.environ.get('MONGOHQ_URL', 'mongodb://127.0.0.1:27017/shortener')
 conn = MongoClient(MONGO_URL)
 db = conn[MONGO_URL.split('/')[-1]]
 
@@ -13,28 +13,30 @@ SERVER_URL = os.environ.get('SERVER_URL', '127.0.0.1:5000')
 app = Flask(__name__)
 
 
-def base_52(num, power=6, result=''):
+def base_61(num, power=6, result=''):
 
     if power < 0:
         return result
 
     if power > 5:
-        return base_52(num % pow(52, power), power - 1)
+        return base_61(num % pow(61, power), power - 1)
 
-    value = num / pow(52, power)
+    value = num / pow(61, power)
 
     if value < 26:
         c = chr(value + 65)
-    else:
+    elif value < 52:
         c = chr(value + 71)
+    else:
+        c = chr(value - 3)
 
-    return base_52(num % pow(52, power), power - 1, result = result + c)
+    return base_61(num % pow(61, power), power - 1, result = result + c)
 
 
 def shorten_url(url):
     m = hashlib.md5()
     m.update(url)
-    path = base_52(int(m.hexdigest(), 16))
+    path = base_61(int(m.hexdigest(), 16))
     return (path, '%s/%s' % (SERVER_URL, path))
 
 
